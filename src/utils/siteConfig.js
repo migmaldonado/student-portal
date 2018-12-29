c68a38d111878431985e2229afb2110d61afe41a
Miguel Maldonado <maldesan@gmail.com>
2018-12-29 17:34:27 -0500

    fix siteconfig

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
-  siteTitle: 'GCN',
-  siteTitleAlt: 'GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
-  publisher: 'Publisher named GCN', // Organization name used for SEO schema
+  siteTitle: 'Student Portal for Data Science Bootcamp',
+  siteTitleAlt: 'Data Science Bootcamp in Mexico City', // This allows an alternative site title for SEO schema.
+  publisher: 'Data Science Academy Mexico', // Organization name used for SEO schema
   siteDescription:
-    'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
-  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+    'A student portal for the Data Science Bootcamp',
+  siteUrl: 'https://www.datascienceacademy.com.mx', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 6, // Number of posts shown on paginated pages
-  author: 'GCN User', // Author for RSS author segment and SEO schema
-  authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
-  userTwitter: '@twitter', // Change for Twitter Cards
+  author: 'Data Science Academy Collective', // Author for RSS author segment and SEO schema
+  authorUrl: 'https://www.datascenceacademy.com.mx', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@DataScienceMX', // Change for Twitter Cards
   shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
@@ -17,5 +17,5 @@ module.exports = {
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#e9e9e9', // Used for Offline Manifest
   themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+  copyright: 'Copyright © 2018 Data Science Academy Mexico', // Copyright string for the RSS feed
 }
